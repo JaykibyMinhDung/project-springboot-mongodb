fcec309e78614b23bc5546b251b15ef264283a71
MinhDungJKB <doanminhdung1729@gmail.com>
2025-03-11 16:50:13 +0700

    add model product

---
+++ src/main/java/com/example/mdbspringbootreactive/model/Product.java
@@ -0,0 +1,98 @@
+package com.example.mdbspringbootreactive.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Document(collection = "product")
+public class Product {
+//    @Id
+//    private String id;
+//    private String title;
+//    private String category_id;
+//    private String description;
+//    private Double price;
+//    private LocalDateTime created_at;
+//    private LocalDateTime updated_at;
+
+    @Id
+    private String id;
+    private String category;
+    private String description;
+    private String image;
+    private Integer price;  // Dùng Integer vì trong MongoDB là Int32
+    private String title;
+
+    public Product() {}
+
+    public Product(String id, String category, String description, String image, Integer price, String title) {
+        this.id = id;
+        this.category = category;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", category='" + category + '\'' +
+                ", description='" + description + '\'' +
+                ", image='" + image + '\'' +
+                ", price=" + price +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
